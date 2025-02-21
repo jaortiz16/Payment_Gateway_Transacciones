fbdc440c610aed5053f83d3a94ed91f72ee4e8d4
Andres <andres.ortiz.h.2001@gmail.com>
2025-02-20 23:07:55 -0500

    upload of the project

--- src/main/java/com/banquito/payment_gateway/transacciones/banquito/service/TransaccionService.java
+++ src/main/java/com/banquito/payment_gateway/transacciones/banquito/service/TransaccionService.java
@@ -13,7 +13,7 @@ import com.banquito.payment_gateway.transacciones.banquito.model.Transaccion;
 import com.banquito.payment_gateway.transacciones.banquito.repository.TransaccionRepository;
 
 import lombok.extern.slf4j.Slf4j;
-
+//test
 @Service
 @Slf4j
 public class TransaccionService {
